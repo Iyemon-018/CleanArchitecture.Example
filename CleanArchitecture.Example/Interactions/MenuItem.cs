a2157dd7c7969ae2f221ac99b15c44c390562dcc
iyemon018 <iyemon018@gmail.com>
2018-11-18 19:29:59 +0900

    画面レイアウトを作成

---
+++ CleanArchitecture.Example/Interactions/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace CleanArchitecture.Example.Interactions
+{
+    public sealed class MenuItem : BindableBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+    }
+}
--- CleanArchitecture.Example/ViewModels/ShellViewModel.cs
+++ CleanArchitecture.Example/ViewModels/ShellViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 using CleanArchitecture.Example.Domain.Services;
+using CleanArchitecture.Example.Interactions;
 using Prism.Commands;
 
 namespace CleanArchitecture.Example.ViewModels
@@ -11,28 +14,14 @@ namespace CleanArchitecture.Example.ViewModels
 
         public ShellViewModel(IDialogService dialogService) : base(dialogService)
         {
-            InformationDialogCommand = new DelegateCommand(() => dialogService.Information(DialogData.Build("Test", "テスト的に情報メッセージを表示しています。表示されていればOKです。")));
-            QuestionDialogCommand = new DelegateCommand(async () =>
-                                                        {
-                                                            var result = await dialogService.Question(DialogData.Build("Test"
-                                                                                                  , $"終了します。{Environment.NewLine}"
-                                                                                                    + $"よろしいですか。"));
-                                                            if (result)
-                                                            {
-                                                                App.Current.Shutdown();
-                                                            }
-                                                        });
-            ErrorDialogCommand = new DelegateCommand(async () =>
-                                                     {
-                                                         await dialogService.Error(DialogData.Build("Test"
-                                                                                                  , "エラーです。これはエラーであることを示しています。"));
-                                                     });
+            MenuItems = new ObservableCollection<MenuItem>(new[]
+                                                           {
+                                                               "GetCurrentDateTime"
+                                                             , "DetailDataList"
+                                                           }
+                                                               .Select(x => new MenuItem {Name = x}));
         }
 
-        public ICommand InformationDialogCommand { get; private set; }
-
-        public ICommand QuestionDialogCommand { get; private set; }
-
-        public ICommand ErrorDialogCommand { get; private set; }
+        public ObservableCollection<MenuItem> MenuItems { get; }
     }
 }
